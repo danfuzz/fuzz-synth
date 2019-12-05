4ff2f7dcdd3cdd06a9f52b94f0d28e114c4a0b77
Dan Bornstein <danfuzz@milk.com>
2019-12-05 14:05:55 -0800

    Stereo and 64-bit float.

--- organelle-node-test/src/audio-test.js
+++ organelle-node-test/src/audio-test.js
@@ -5,6 +5,7 @@ import { promisify } from 'util';
 import { Endianness } from './Endianness.js';
 
 const SAMPLE_RATE_HZ    = 44100;
+const CHANNELS          = 2;
 const FREQ_HZ           = 130.8128; // One octave below middle C.
 const WAVELEN_SAMPLES   = Math.round(SAMPLE_RATE_HZ / FREQ_HZ);
 const MSEC_PER_WAVE     = SAMPLE_RATE_HZ / WAVELEN_SAMPLES;
@@ -12,9 +13,9 @@ const MSEC_PER_WAVE     = SAMPLE_RATE_HZ / WAVELEN_SAMPLES;
 const ALSA_AUDIO_OUTPUT_COMMAND = Object.freeze([
   'aplay',
   '--buffer-time=50000', // 50 msec
-  '--channels=1',
+  `--channels=${CHANNELS}`,
   '--duration=0', // `0` means "forever."
-  `--format=${Endianness.isLittleEndian() ? 'U32_LE' : 'U32_BE'}`,
+  `--format=${Endianness.isLittleEndian() ? 'FLOAT64_LE' : 'FLOAT64_BE'}`,
   '--nonblock',
   //'--quiet',
   `--rate=${SAMPLE_RATE_HZ}`
@@ -23,10 +24,10 @@ const ALSA_AUDIO_OUTPUT_COMMAND = Object.freeze([
 const SOX_AUDIO_OUTPUT_COMMAND = Object.freeze([
   'sox',
   //'-V3',
-  '--bits=32',
-  '--buffer=2205',
-  '--channels=1',
-  '--encoding=unsigned-integer',
+  '--bits=64',
+  '--buffer=2205', // Docs say "bytes" but appears to actually be samples.
+  `--channels=${CHANNELS}`,
+  '--encoding=floating-point',
   `--endian=${Endianness.isLittleEndian() ? 'little' : 'big'}`,
   '--ignore-length',
   '--no-show-progress', // a/k/a `--quiet` or `--silent` on most utilities.
@@ -36,13 +37,15 @@ const SOX_AUDIO_OUTPUT_COMMAND = Object.freeze([
   '--default-device' // Write to the default device.
 ]);
 
-const INC_PER_SAMPLE = (0x100000000 * 0.75) / WAVELEN_SAMPLES;
-const singleCycleSaw = new Uint32Array(WAVELEN_SAMPLES);
+const INC_PER_SAMPLE = (2 * 0.80) / WAVELEN_SAMPLES;
+const singleCycleSaw = new Float64Array(WAVELEN_SAMPLES * CHANNELS);
 const waveBuf = new Uint8Array(singleCycleSaw.buffer);
 for (let i = 0; i < WAVELEN_SAMPLES; i++) {
-  const value = (i * INC_PER_SAMPLE) + ((1 << 32) * 0.05);
-  singleCycleSaw[i] = value;
-  console.log('====', singleCycleSaw[i], value);
+  const value = (i * INC_PER_SAMPLE) - (1 - 0.1);
+  for (let j = 0; j < CHANNELS; j++) {
+    singleCycleSaw[(i * CHANNELS) + j] = value;
+  }
+  console.log('====', singleCycleSaw[i * CHANNELS], value);
 }
 console.log(`==== Single cycle is ${WAVELEN_SAMPLES} samples.`);
 
